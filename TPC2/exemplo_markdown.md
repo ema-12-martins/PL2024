3f266025b44fb36652628d38781f746e33b856fc
ema-12-martins <a97678@alunos.uminho.pt>
2024-02-22 20:50:36 +0000

    so falta as listas

--- TPC2/exemplo_markdown.md
+++ TPC2/exemplo_markdown.md
@@ -7,3 +7,6 @@ vdfk-vmk-dfmv--mldfm--vldfmvlmdflv
 1. dsndc
 2. kcdnsvkd
 3. sdnkcn
+
+[girassois bonitos](https://img.joomcdn.net/1c124d8f6cb14ba785dae70e7d61a2781791d503_original.jpeg)
+![girassois bonitos](girassol.jpeg)
--- TPC2/script.py
+++ TPC2/script.py
@@ -23,17 +23,31 @@ def main(path):
     with open(path, 'r') as arquivo:
         conteudo = arquivo.read()
 
+        #Titulos
         conteudo = re.sub(r"#{6}(.*)\n", r"<h6>\1</h6>", conteudo)
         conteudo = re.sub(r"#{5}(.*)\n", r"<h5>\1</h5>", conteudo)
         conteudo = re.sub(r"#{4}(.*)\n", r"<h4>\1</h4>", conteudo)
         conteudo = re.sub(r"#{3}(.*)\n", r"<h3>\1</h3>", conteudo)
         conteudo = re.sub(r"#{2}(.*)\n", r"<h2>\1</h2>", conteudo)
         conteudo = re.sub(r"#(.*)\n", r"<h1>\1</h1>", conteudo)
+
+        #Italicos e Bolds
         conteudo = re.sub(r"\*\*(.*)\*\*", r"<b>\1</b>", conteudo, flags=re.DOTALL) #Para fazer com que o . a qualquer caracter, inclusive o \n
         conteudo = re.sub(r"--(.*)--", r"<b>\1</b>", conteudo, flags=re.DOTALL) 
         conteudo = re.sub(r"\*(.*)\*", r"<i>\1</i>", conteudo, flags=re.DOTALL) 
         conteudo = re.sub(r"-(.*)-", r"<i>\1</i>", conteudo, flags=re.DOTALL) 
+
+        #Imagens
+        conteudo = re.sub(r"\!\[(.*?)\]\((.*?)\)",  r"<img src='\2' alt='\1'>", conteudo, flags=re.DOTALL) 
+
+        #Links
+        conteudo = re.sub(r"\[(.*?)\]\((.*?)\)", r"<a href='\2'>\1</a>", conteudo, flags=re.DOTALL) 
+
         
+
+        
+
+
 
 
         print(conteudo)

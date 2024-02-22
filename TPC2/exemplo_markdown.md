cf8ff1fccc12f550482455c79e08d82554cc2247
ema-12-martins <a97678@alunos.uminho.pt>
2024-02-22 20:35:47 +0000

    parte inicial feita

---
+++ TPC2/exemplo_markdown.md
@@ -0,0 +1,9 @@
+# Titulo principal
+### Segundo titulo
+sdnjvbreibvierbifdnvknkdfvkdfkv
+dfnivnidfnv**kdfvkk**dfmvk*mdkm*dvkmfd
+vdfk-vmk-dfmv--mldfm--vldfmvlmdflv
+
+1. dsndc
+2. kcdnsvkd
+3. sdnkcn
---
+++ TPC2/script.py
@@ -0,0 +1,43 @@
+import re
+import sys
+
+
+def main(path):
+    #Cabecalho inicial do HTML
+    html='''
+    <!DOCTYPE htlm>
+    <html>
+    <head>
+        <title>EngWeb2024</title>
+        <meta charset="UTF-8">
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+
+    '''
+
+    with open(path, 'r') as arquivo:
+        conteudo = arquivo.read()
+
+        conteudo = re.sub(r"#{6}(.*)\n", r"<h6>\1</h6>", conteudo)
+        conteudo = re.sub(r"#{5}(.*)\n", r"<h5>\1</h5>", conteudo)
+        conteudo = re.sub(r"#{4}(.*)\n", r"<h4>\1</h4>", conteudo)
+        conteudo = re.sub(r"#{3}(.*)\n", r"<h3>\1</h3>", conteudo)
+        conteudo = re.sub(r"#{2}(.*)\n", r"<h2>\1</h2>", conteudo)
+        conteudo = re.sub(r"#(.*)\n", r"<h1>\1</h1>", conteudo)
+        conteudo = re.sub(r"\*\*(.*)\*\*", r"<b>\1</b>", conteudo, flags=re.DOTALL) #Para fazer com que o . a qualquer caracter, inclusive o \n
+        conteudo = re.sub(r"--(.*)--", r"<b>\1</b>", conteudo, flags=re.DOTALL) 
+        conteudo = re.sub(r"\*(.*)\*", r"<i>\1</i>", conteudo, flags=re.DOTALL) 
+        conteudo = re.sub(r"-(.*)-", r"<i>\1</i>", conteudo, flags=re.DOTALL) 
+        
+
+
+        print(conteudo)
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1])

da57effd560682f70832baf2c6e516fb3731683f
ema-12-martins <a97678@alunos.uminho.pt>
2024-02-23 21:31:59 +0000

    Esta feito

--- TPC3/script.py
+++ TPC3/script.py
@@ -11,12 +11,17 @@ def main():
     i=0
     soma=0
     estado="on"
+    estado_anterior="on"
     while i<len(divisao):
-
         if(estado=="on"):
             soma+=sum(map(int,re.findall(r'[+\-]?\d+',divisao[i])))
+            estado_anterior=="on"
         elif(estado=="="):
             print(soma)
+            if(estado_anterior=="on"):
+                soma+=sum(map(int,re.findall(r'[+\-]?\d+',divisao[i])))
+        elif(estado=="off"):
+            estado_anterior=="off"
 
         if(i<len(divisorios)):
             estado=divisorios[i]
